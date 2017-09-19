71fdb5d9c4936991c1434a2339354e4112f8cee9
Alex Milbert <alex.milbert@gmail.com>
2017-09-19 10:54:15 -0700

    objects updated

--- 100_lines.js
+++ 100_lines.js
@@ -7,28 +7,35 @@ let num = Math.floor(Math.random() * 5);
 let sock = {
   color: "white",
   comesInMultiples: "yes",
+  shape: "semi-cylindrical",
 }
 
 let rabbit = {
   color: "white",
   comesInMultiples: "no",
+  shape: "varied",
 }
 
 let basketball = {
   color: "brown",
   comesInMultiples: "no",
+  shape: "spherical",
 }
 
 let spinach = {
   color: "green",
   comesInMultiples: "yes",
+  shape: "round/oblong",
 }
 
 let mountain = {
   color: "white",
   comesInMultiples: "no",
+  shape: "cone",
 }
 
 function threeQuestions() {
 
 }
+
+// guess the NBA player.  NBA player objects, pick a question, then enter a guss.
