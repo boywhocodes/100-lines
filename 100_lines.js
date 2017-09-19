db4ebae6049b05ec11dc2f34df9f03622c8c7736
Alex Milbert <alex.milbert@gmail.com>
2017-09-19 10:34:26 -0700

    objects created

--- 100_lines.js
+++ 100_lines.js
@@ -1,3 +1,29 @@
-function test() {
-  
+// nested functions
+// basketball will be in first execution context.  child functions will be called internally
+
+
+let num = Math.floor(Math.random() * 5);
+
+let sock = {
+  color: "white"
+}
+
+let rabbit = {
+
+}
+
+let basketball = {
+
+}
+
+let spinach = {
+
+}
+
+let mountain = {
+
+}
+
+function threeQuestions() {
+
 }
