30d5bfe3e8fe7b55f51493403cb3fd7a1f46f565
Alex Milbert <alex.milbert@gmail.com>
2017-10-01 21:32:49 -0700

    calc w funcs

--- 100_lines.js
+++ 100_lines.js
@@ -389,17 +389,43 @@ function gap(g, m, n) {
 
 // Calculating with Functions
 // function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+function one() {
+  return 1
+}
+function two() {
+  return 2
+}
+function three() {
+  return 3
+}
+function four() {
+  return 4
+}
+function five() {
+  return 5
+}
+function six() {
+  return 6
+}
+function seven() {
+  return 7
+}
+function eight() {
+  return 8
+}
+function nine() {
+  return 9
+}
 
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+function plus() {
+  return +
+}
+function minus() {
+  return -
+}
+function times() {
+  return *
+}
+function dividedBy() {
+  return /
+}
